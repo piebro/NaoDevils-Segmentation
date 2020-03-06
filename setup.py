5b110611d73af7ab02758bf6a7f8009803037381
Piet Brömmel <piet.broemmel@instana.com>
2020-03-06 14:19:29 +0100

    fix installation

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ setup(name="robocup_data_loader",
       platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
       license="MIT",
       url="https://github.com/piebro/robocup-dataset-loader",
-      packages=find_packages(exclude=["test"]),
+      packages=find_packages(),
       install_requires=[
-            "png",
-            "itertools",
-            "pycocotools",
-            "opencv-python",
-            "numpy",
-            "google.protobuf",
-            "imgaug",
-            "matplotlib"]
+            "png==0.0.20",
+            "itertools==8.2.0",
+            "pycocotools==2.0.0",
+            "opencv-python==4.1.2.30",
+            "numpy==1.17.5",
+            "google==2.0.3",
+            "imgaug==0.2.9",
+            "matplotlib==3.1.3"]
 )
