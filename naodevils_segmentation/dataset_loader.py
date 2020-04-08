301d0d47ed640533977f920652e2a435b7ffbd40
Piet Brömmel <piet.broemmel@instana.com>
2020-04-08 13:26:29 +0200

    updated dataloader to new dataset structure

--- naodevils_segmentation/dataset_loader.py
+++ naodevils_segmentation/dataset_loader.py
@@ -3,77 +3,86 @@ import io
 import os
 import json
 
-import png
+#import png
 import cv2
 from pycocotools.coco import COCO
-from google.protobuf.json_format import MessageToJson
+#from google.protobuf.json_format import MessageToJson
 import numpy as np
 
-import naodevils_segmentation.imageLabelData_pb2 as imageLabelData_pb2
+#import naodevils_segmentation.imageLabelData_pb2 as imageLabelData_pb2
 
 
-def get_data_list(dataset_folder, dataset_type=None, dataset_validation_size=0.2, ran_seed=42):
+def get_data_list(dataset_folder, dataset_validation_size=0.2, ran_seed=42):
   """
   Get a list of all training or validation img_path, annontation, meta_info
   """
-  folder_names = []
-  for file_name in os.listdir(dataset_folder):
-    if file_name.endswith(".json"):
-      folder_names.append(file_name[:-5])
+  #annotation_paths = []
+  #for json_file_name in os.listdir(os.path.join(dataset_folder, "annotations")):
+  #  annotation_paths.append(os.path.splitext(json_file_name)[0])
 
+  annotations_path = os.path.join(dataset_folder, "annotations")
+  images_path = os.path.join(dataset_folder, "images")
 
+  print(annotations_path)
+  
   img_paths_annotations = []
-  for folder_name in folder_names:
+  for json_file_name in os.listdir(annotations_path):
+    
     # to surpress the coco lib prints
     save_stdout = sys.stdout
     sys.stdout = io.StringIO()
-    coco = COCO(os.path.join(dataset_folder, folder_name + ".json"))
+    coco = COCO(os.path.join(annotations_path, json_file_name))
     # to use the normal stdout again
     sys.stdout = save_stdout
 
-    img_ids = sorted(coco.imgs.keys())
-    for img_id in img_ids:
+    img_folder_path = os.path.join(images_path, os.path.splitext(json_file_name)[0])
+    print(img_folder_path)
+    
+    for img_id in sorted(coco.imgs.keys()):
       img_file_name = coco.loadImgs(img_id)[0]['file_name']
-      img_path = os.path.join(folder_name, img_file_name)
-      img_path = os.path.join(dataset_folder, img_path)
+      img_path = os.path.join(img_folder_path, img_file_name)
 
       data_entry = {
         "img_path": img_path,
         "annotation": coco.loadAnns(coco.getAnnIds(img_id))
       }
-      
-      data_entry["meta_info"] = get_image_meta_infos(img_path)
+      #data_entry["meta_info"] = get_image_meta_infos(img_path)
       
       img_paths_annotations.append(data_entry)
   
   np.random.seed(ran_seed)
   np.random.shuffle(img_paths_annotations)
   num_of_val_img = round(len(img_paths_annotations)*dataset_validation_size)
-  if(dataset_type=="training"):
-    img_paths_annotations = img_paths_annotations[:-num_of_val_img]
-  if(dataset_type=="validation"):
-    img_paths_annotations = img_paths_annotations[-num_of_val_img:]
-  
-  return img_paths_annotations
 
-def get_image_meta_infos(img_path):
-  """
-  get brotobuf metainfo in png at img_path
-  """
-  protobuf_chunk = read_label_chunk(img_path)
-  data = imageLabelData_pb2.ImageLabelData()
-  data.ParseFromString(protobuf_chunk)
-  data_json = MessageToJson(data)
-  return json.loads(data_json)
+  dataset = {
+      "training": img_paths_annotations[:-num_of_val_img],
+      "validation": img_paths_annotations[-num_of_val_img:]
+  }
 
-def read_label_chunk(img_path):
-    """
-    read the chunk of the png image
-    """
-    p = png.Reader(img_path)
-    for chunk_name, chunk_data in p.chunks():
-        if chunk_name == b'laBl':
-            return chunk_data
+  # get the automatically labeld annotations
+
+  return dataset
+
+
+
+# def get_image_meta_infos(img_path):
+#   """
+#   get brotobuf metainfo in png at img_path
+#   """
+#   protobuf_chunk = read_label_chunk(img_path)
+#   data = imageLabelData_pb2.ImageLabelData()
+#   data.ParseFromString(protobuf_chunk)
+#   data_json = MessageToJson(data)
+#   return json.loads(data_json)
+
+# def read_label_chunk(img_path):
+#     """
+#     read the chunk of the png image
+#     """
+#     p = png.Reader(img_path)
+#     for chunk_name, chunk_data in p.chunks():
+#         if chunk_name == b'laBl':
+#             return chunk_data
 
 
 def get_image_array(image_input, width, height, imgNorm="sub_mean",
